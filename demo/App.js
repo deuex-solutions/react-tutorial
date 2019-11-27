e9608eba7c490aca2365e0ff8d08d5859e5d21e7
Chirag Madlani <chiragmadlani7695@gmail.com>
2019-11-27 12:02:12 +0530

    Add demo

---
+++ demo/App.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import {ReactTutorial} from '../src';
+
+//create your forceUpdate hook
+function useForceUpdate(){
+  const [value, setValue] = useState(true); //boolean state
+  return () => setValue(!value); // toggle the state to force render
+}
+let tourPlaying = false;
+function App() {
+  const forceUpdate = useForceUpdate();
+  const [typeValue, setTypeValue] = useState('');
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <div>
+        <button id={'btn6'} onClick={() => {
+          tourPlaying = true;
+          forceUpdate(); 
+        }}>start tour</button>
+      </div>
+      <div>
+        <button id={'btn1'}>button 1</button>
+      </div>
+      <div>
+        <span id={'btn2'}>button 2</span>
+      </div>
+      <div>
+        <input type="text" name="name" id='name1' value={typeValue} onChange={(e) => setTypeValue(e.target.value)} />
+      </div>
+      
+        <ReactTutorial 
+          steps={[
+            {
+                instructionText: 'Click on btn',
+                actionType: 1,
+                position: 'top',
+                selector: "#btn1"
+            },
+            {
+                instructionText: 'double click this item',
+                actionType: 2,
+                position: 'top',
+                selector: "#btn2"
+            },
+            {
+                instructionText: 'Type "Hello world"',
+                actionType: 3,
+                position: 'top',
+                text:'Hello world',
+                selector: "#name1"
+            }
+          ]}
+          playTour={tourPlaying}
+          onRequestClose={() => {
+            tourPlaying=false;
+            forceUpdate();
+          }}
+        />
+    </div>
+  );
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: ['babel-polyfill', './demo/index.js'],
+    devtool: 'sourcemap',
     module: {
         rules: [
             {
                 test: /\.js$/,
+                exclude: [/node_modules/],
                 use:{
                     loader: "babel-loader",
                 },
             },
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: 'html-loader',
+                    options: { minimize: true },
+                  },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: './demo/public/index.html',
+          filename: './index.html',
+        }),
+    ],
     output: {
         path: path.resolve(__dirname, 'dist/'),
         publicPath: '',
-        filename: 'build.js',
+        filename: '[name].js',
         libraryTarget: 'umd'
     }
 };
