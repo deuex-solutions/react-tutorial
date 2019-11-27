2d989ef34419d4ff1786c453b47db9193e82c4ea
Chirag Madlani <chiragmadlani7695@gmail.com>
2019-11-27 14:57:50 +0530

    Update library & build process

---
+++ webpack.prod.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['./src/index.js'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use:{
+                    loader: "babel-loader",
+                },
+            },
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '',
+        filename: '[name].js',
+        libraryTarget: 'umd'
+    },
+    externals: [
+        "react",
+        "react-dom",
+        "styled-components",
+        "lodash",
+        "scroll-smooth",
+        "scrollparent"
+    ],
+};
